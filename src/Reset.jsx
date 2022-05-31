4534e63312751ab57ff8a719a2ee1ebd86d36a83
Lourens Schep <lourensschep@gmail.com>
2022-05-31 16:01:14 -0300

    Made use of ipcAsync and disable button upon reset

--- src/Reset.jsx
+++ src/Reset.jsx
@@ -1,23 +1,28 @@
 import { Button } from '@getflywheel/local-components';
-import { ipcRenderer } from 'electron';
+import * as LocalRenderer from '@getflywheel/local/renderer';
 import {
     useParams
 } from "react-router-dom";
+import { useState } from 'react';
 import { EVENTS } from './constants';
 
 export function Reset() {
     const { siteID } = useParams();
+    const [ resetting, setResetting ] = useState( false );
 
     const onResetSite = () => {
-        ipcRenderer.send(
-            EVENTS.resetSite,
-            siteID,
-        );
+        setResetting( true )
+        LocalRenderer.ipcAsync(EVENTS.resetSite, siteID).then((result) => {
+            setResetting(false);
+        }, (error) => {
+            setResetting(false);
+        });
+
     }
 
     return <div style={{ flex: '1', overflowY: 'auto', margin: '10px' }}>
         <div>
-            <Button onClick={onResetSite}>Reset site</Button>
+            <Button onClick={onResetSite} disabled={ resetting }>{ resetting ? 'Resetting' : 'Reset site' }</Button>
         </div>
     </div>
 }
--- src/main.ts
+++ src/main.ts
@@ -3,49 +3,50 @@ import * as LocalMain from '@getflywheel/local/main';
 import { EVENTS } from './constants';
 
 export default function (context) {
-	const { electron } = context;
-	const { ipcMain } = electron;
+	LocalMain.addIpcAsyncListener(
+		EVENTS.resetSite,
+		async (siteId: string) => {
+			LocalMain.getServiceContainer().cradle.localLogger.log('info', 'Triggering resetSite for site: ' + siteId);
 
-	ipcMain.on(EVENTS.resetSite, async (event, siteId) => {
-		LocalMain.getServiceContainer().cradle.localLogger.log('info', 'Triggering resetSite for site: ' + siteId);
+			const site = LocalMain.getServiceContainer().cradle.siteData.getSite(siteId);
 
-		const site = LocalMain.getServiceContainer().cradle.siteData.getSite(siteId);
-
-		let adminUsers = await LocalMain.getServiceContainer().cradle.wpCli.run(site, ['user',
-			'list',
-			'--role=administrator',
-			'--format=json'
-		]);
-		adminUsers = adminUsers ? JSON.parse(adminUsers) : [];
-		const adminUser = adminUsers.length > 0 ? adminUsers[0] : {};
-
-		try {
-			await LocalMain.getServiceContainer().cradle.wpCli.run(site, [
-				'db',
-				'reset',
-				'--yes',
-				'--defaults',
+			let adminUsers = await LocalMain.getServiceContainer().cradle.wpCli.run(site, ['user',
+				'list',
+				'--role=administrator',
+				'--format=json'
 			]);
-		} catch (error) {
+			adminUsers = adminUsers ? JSON.parse(adminUsers) : [];
+			const adminUser = adminUsers.length > 0 ? adminUsers[0] : {};
 
-		}
-		LocalMain.getServiceContainer().cradle.localLogger.log('info', 'Command "wp db reset" finished.');
+			try {
+				await LocalMain.getServiceContainer().cradle.wpCli.run(site, [
+					'db',
+					'reset',
+					'--yes',
+					'--defaults',
+				]);
+			} catch (error) {
+				throw error;
+			}
+			LocalMain.getServiceContainer().cradle.localLogger.log('info', 'Command "wp db reset" finished.');
 
-		// Run WP-CLI command.
-		try {
-			await LocalMain.getServiceContainer().cradle.wpCli.run(site, [
-				'core',
-				'install',
-				'--url=' + site.url,
-				'--title=' + site.domain,
-				'--admin_user=' + adminUser['user_login'] || 'admin',
-				'--admin_password=password',
-				'--admin_email=' + adminUser['user_email'] || 'dev-email@flywheel.local',
-				'--skip-email',
-			]);
-		} catch (error) {
+			// Run WP-CLI command.
+			try {
+				await LocalMain.getServiceContainer().cradle.wpCli.run(site, [
+					'core',
+					'install',
+					'--url=' + site.url,
+					'--title=' + site.domain,
+					'--admin_user=' + adminUser['user_login'] || 'admin',
+					'--admin_password=password',
+					'--admin_email=' + adminUser['user_email'] || 'dev-email@flywheel.local',
+					'--skip-email',
+				]);
+			} catch (error) {
+				throw error;
 
-		}
-		LocalMain.getServiceContainer().cradle.localLogger.log('info', 'Command "wp core install" finished.');
-	});
+			}
+			LocalMain.getServiceContainer().cradle.localLogger.log('info', 'Command "wp core install" finished.');
+			return true;
+		});
 }

5746f06afe2c19fc59c7079c319ca1261d999e4a
Lourens Schep <lourensschep@gmail.com>
2022-05-31 15:23:12 -0300

    Add resetSite event and log action in main.ts

--- src/Reset.jsx
+++ src/Reset.jsx
@@ -1,9 +1,18 @@
 import { Button } from '@getflywheel/local-components';
+import { ipcRenderer } from 'electron';
+import {
+    useParams
+} from "react-router-dom";
+import { EVENTS } from './constants';
 
 export function Reset() {
+    const { siteID } = useParams();
 
     const onResetSite = () => {
-        console.log('resetSite');
+        ipcRenderer.send(
+            EVENTS.resetSite,
+            siteID,
+        );
     }
 
     return <div style={{ flex: '1', overflowY: 'auto', margin: '10px' }}>

32e896b1cf73b725e5ef4c5fe9439904c6175ee2
Lourens Schep <lourensschep@gmail.com>
2022-05-31 15:34:29 -0300

    Add reset site logic to resetSite listener in main using wpCli

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,44 @@ export default function (context) {
 
 	ipcMain.on(EVENTS.resetSite, async (event, siteId) => {
 		LocalMain.getServiceContainer().cradle.localLogger.log('info', 'Triggering resetSite for site: ' + siteId);
+
+		const site = LocalMain.getServiceContainer().cradle.siteData.getSite(siteId);
+
+		let adminUsers = await LocalMain.getServiceContainer().cradle.wpCli.run(site, ['user',
+			'list',
+			'--role=administrator',
+			'--format=json'
+		]);
+		adminUsers = adminUsers ? JSON.parse(adminUsers) : [];
+		const adminUser = adminUsers.length > 0 ? adminUsers[0] : {};
+
+		try {
+			await LocalMain.getServiceContainer().cradle.wpCli.run(site, [
+				'db',
+				'reset',
+				'--yes',
+				'--defaults',
+			]);
+		} catch (error) {
+
+		}
+		LocalMain.getServiceContainer().cradle.localLogger.log('info', 'Command "wp db reset" finished.');
+
+		// Run WP-CLI command.
+		try {
+			await LocalMain.getServiceContainer().cradle.wpCli.run(site, [
+				'core',
+				'install',
+				'--url=' + site.url,
+				'--title=' + site.domain,
+				'--admin_user=' + adminUser['user_login'] || 'admin',
+				'--admin_password=password',
+				'--admin_email=' + adminUser['user_email'] || 'dev-email@flywheel.local',
+				'--skip-email',
+			]);
+		} catch (error) {
+
+		}
+		LocalMain.getServiceContainer().cradle.localLogger.log('info', 'Command "wp core install" finished.');
 	});
 }
